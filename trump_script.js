921e33147fb6e0b783072299fa2f788cfb960253
Sagar Patil <sagar010797@gmail.com>
2017-05-29 18:50:22 +0530

    added script for word replacement

---
+++ trump_script.js
@@ -0,0 +1,44 @@
+walk(document.body);
+
+function walk(node) 
+{
+	
+	var child, next;
+	
+	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
+	    || node.classList.indexOf('ace_editor') > -1) {
+		return;
+	}
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bMr. Trump\b/g, "Crime master Gogo");
+	v = v.replace(/\bDonald Trump\b/g, "Crime master Gogo");
+
+	textNode.nodeValue = v;
+}
+
+
+
